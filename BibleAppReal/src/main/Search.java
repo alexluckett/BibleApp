b2b1dc48e49408ca0148f70b0a26e640efd1bdc9
ashleybridgwood <ashley_bridgwood@hotmail.co.uk>
2014-11-06 15:07:35 +0000

    Changes to the search option

--- BibleAppReal/src/main/Search.java
+++ BibleAppReal/src/main/Search.java
@@ -1,10 +1,14 @@
 package main;
 
+
 public class Search {
 	
 	private int wordCount;
+	private String wordToSearch;
 	
 	public Search(String word){
-		
+		wordCount = 0;
+		wordToSearch = word;
 	}
+	
 }
