0ec4ab85a46ab939ebb74fe1968f1634149ebbab
Charandeep <charandeep.rai@gmail.com>
2014-11-08 19:58:56 +0000

    Just testing my push

--- BibleAppReal/src/main/Chapter.java
+++ BibleAppReal/src/main/Chapter.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 public class Chapter {
 	private String title;
 	private String description;
+	private String pslamdescription; //Just a test thing to check if i can push.
 	private Collection<String> verses;
 	
 	public Chapter() {
