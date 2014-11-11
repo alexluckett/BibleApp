beea7ba83219ef9f4b551af2e78c5084811242fc
Alex Luckett <alexgluckett@gmail.com>
2014-11-11 11:52:11 +0000

    Constructor and getter methods

--- BibleAppReal/src/main/Chapter.java
+++ BibleAppReal/src/main/Chapter.java
@@ -5,11 +5,23 @@ import java.util.Collection;
 public class Chapter {
 	private String title;
 	private String description;
-	private String pslamdescription; //Just a test thing to check if i can push.
-	private Collection<String> verses;
+	private Collection<Verse> verses;
 	
-	public Chapter() {
-		// TODO Auto-generated constructor stub
+	public Chapter(String title, String description) {
+		this.title = title;
+		this.description = description;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public Collection<Verse> getVerses() {
+		return verses;
 	}
 	
 }
