aaa42ee33dbb6179366075f504519f94eea5255f
Alex Luckett <alexgluckett@gmail.com>
2014-11-11 11:28:48 +0000

    Test

--- BibleAppReal/src/main/BibleApp.java
+++ BibleAppReal/src/main/BibleApp.java
@@ -3,7 +3,7 @@ package main;
 import java.util.Collection;
 
 public class BibleApp {
-	private Collection<Chapter> books;
+	private Collection<Chapter> books; // test comment for a sec lel
 	
 	public BibleApp() {
 		// TODO Auto-generated constructor stub
