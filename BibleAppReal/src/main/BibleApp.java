ab809baf1fb9942a48e98f48b2137126de60d25a
Alex Luckett <alexgluckett@gmail.com>
2014-11-13 20:45:19 +0000

    Fixed error parsing 58+ files.

--- BibleAppReal/src/main/BibleApp.java
+++ BibleAppReal/src/main/BibleApp.java
@@ -5,34 +5,35 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.text.ParseException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+
 
 /**
- * Completely unoptimised at this point. Did as a very basic concept of reading in the files.
+ * Main class for bible app. Will include ability to load in bible,
+ * search for text and go to certain chapters/verses via command line interface.
  * 
  * @author Alex Luckett
  * @author Ashley Bridgwood
  *
  */
 public class BibleApp {
+	private static File[] bookNames = (new File("data")).listFiles(); // retrieves a list of all files within the data folder (source files)
 	
-	private static File destinationFolder = new File("data");
-	private static File[] bookNames = destinationFolder.listFiles();
-	//private static String[] books = {"1John.txt", "2Kings.txt", "3John.txt", "Colossians.txt", "1Timothy.txt", "Exodus.txt","1Chronicles.txt", "Amos.txt", "Hosea.txt" };
+	private static List<Book> parsedBooks = new ArrayList<Book>(66); // list of all fully parsed books
 	
-	public BibleApp() { }
+	public BibleApp() {}
 	
 	public static void main(String[] args) {
-		
 		long startTime = System.currentTimeMillis();
 		
-		for(int i = 0; i < /*bookNames[i].length()*/ 58; i++){
+		for(int i = 0; i < bookNames.length; i++) {
 			System.out.println("Current Book: " + bookNames[i].getName());
 			readInFile(bookNames[i].getName());
 		}
 		
-		//for(int i = 0; i < books.length; i++){
-			//readInFile(books[i]);
-		//}
+		//readInFile("2Kings.txt"); // 2 kings has a description. Here for debugging purposes - uncomment this and comment above to use.
 		
 		long endTime = System.currentTimeMillis();
 				
@@ -42,46 +43,53 @@ public class BibleApp {
 	private static void readInFile(String fileName) {
 		try {
 			BufferedReader reader = new BufferedReader(new FileReader("data/" + fileName));
-			System.out.println("Processing: " + fileName);				
-			long startTime = System.currentTimeMillis();
+			System.out.println("Processing: " + fileName);			
 			
 			Book book = new Book(reader.readLine()); // first line is always the book title
-			
-			String newLine = reader.readLine();
+						
 			int verseNumber = 1;
-			
 			int chapterNumber = 0;
 			
-			while(newLine != null) {
-				if(newLine.startsWith("CHAPTER") || newLine.startsWith("PSALM")) {			
+			String nextLine = reader.readLine();
+			
+			while(nextLine != null) {
+				if(nextLine.startsWith("CHAPTER") || nextLine.startsWith("PSALM")) {			
 					chapterNumber++;
 					verseNumber = 1;
-				} else if(!newLine.equals("")) {
-					book.addVerse(parseVerse(newLine, verseNumber, chapterNumber));
+				} else if(!nextLine.equals("")) {
+					book.addVerse(parseVerse(nextLine, verseNumber, chapterNumber));
 					verseNumber++;
 				}
 				
-				newLine = reader.readLine();
-
+				nextLine = reader.readLine();
 			}
 			
-			long endTime = System.currentTimeMillis();
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ParseException e) {
-			e.printStackTrace();
+			parsedBooks.add(book);
+		} catch (Exception e) {
+			System.err.println("Error reading bible files. Please ensure package is up to date.");
+			//e.printStackTrace();
 		}
 	}
 	
-	private static Verse parseVerse(String line, int verseNumber, int chapterNumber) throws ParseException {		
-		String verseContent = line.substring(line.indexOf(" "));
+	private static Verse parseVerse(String line, int verseNumber, int chapterNumber) throws ParseException {
+		int firstWordEnd = line.indexOf(" ");
+		
+		String description = line.substring(0, firstWordEnd);
+		String verseContent = line.substring(firstWordEnd); // remove verse number (always first string - we already know it at this point
 		
 		Verse verse = new Verse(verseNumber, chapterNumber);
 		verse.addWord(verseContent);
 		
-		//System.out.println("Verse " + verseNumber + " parsed.");
+		/* debugging only - prints out contents of parsed verse.
+		 * 
+		System.out.println("Verse " + verseNumber + " parsed.");
+		System.out.println("Verse content: ");
+		for(String word : verse.getWords()) {
+			System.out.println(word + " ");
+		}
+		System.out.println();
+		System.out.println();
+		*/
 		
 		return verse;
 	}
