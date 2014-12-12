301f808e0a0cde0f66ca61066ef09add36f05ea2
Alex Luckett <alexgluckett@gmail.com>
2014-12-12 10:52:33 +0000

    Removed unused constant, improved documentation.

--- BibleAppSearchOptimised/src/main/Constants.java
+++ BibleAppSearchOptimised/src/main/Constants.java
@@ -6,12 +6,35 @@ package main;
  * @author Alexander Luckett <lucketta@aston.ac.uk>
  */
 public class Constants {
+	/**
+	 * Maximum number of books in the bible. Used to initialise collection holding books.
+	 * 
+	 * 66 books in total.
+	 */
 	public static final int MAX_BOOKS = 66;
-	public static final int MAX_WORDS = 0;
 	
-	public static final int TOTAL_WORDS = 12800;
-	public static final int TOTAL_WORDS_LOADFACTOR = 1;
+	/**
+	 * Initial number of chapters to initialise collections with.
+	 */
+	public static final int CHAPTERS_INITIAL_AMOUNT = 10;
 	
-	public static final int CHAPTERS_INITIAL_AMOUNT = 10; // TODO why is this 10? 10 is default ArrayList amount anyway
+	/**
+	 * Initial number of verses to initialise collections with.
+	 * 
+	 * Testing showed ~50 verses was more efficient in unbounded collections.
+	 */
 	public static final int VERSES_INITIAL_AMOUNT = 50;
+	
+	/**
+	 * Total unique words within the bible text files. Used to initialise the WordMap for efficiency.
+	 */
+	public static final int TOTAL_WORDS = 12800;
+	
+	/**
+	 * Size as a decimal (0-1) in which the WordMap should double in capacity. 
+	 * 
+	 * Since we know how many total words are in the bible (@see TOTAL_WORDS), 
+	 * we can wait until the collection is full before expanding.
+	 */
+	public static final int TOTAL_WORDS_LOADFACTOR = 1;
 }
